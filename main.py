5e4b7f23e1eead111933aeb8b761f07f8ca5d5bb
spiritual <venommortalx@gmail.com>
2023-05-11 08:51:49 -0400

    migrate to js to enter prompts and get chatgpt's answers

--- main.py
+++ main.py
@@ -8,38 +8,57 @@ from selenium.webdriver.support.wait import WebDriverWait
 import time
 import selenium.webdriver.support.expected_conditions as EC
 
+
 def create_webdriver():
     options = webdriver.ChromeOptions()
-    options.add_argument('user-data-dir=C:/Users/Andrew/AppData/Local/Google/Chrome/User Data')
+    options.add_argument(
+        'user-data-dir=C:/Users/Andrew/AppData/Local/Google/Chrome/User Data')
     options.add_argument('profile-directory=Default')
     driver = webdriver.Chrome(options=options)
     return driver
 
+
 def find_current_event():
     driver = create_webdriver()
-    #driver.get("https://www.cnn.com/POLITICS/")
-    #driver.find_element(By.XPATH, "(//h3[@class='cd__headline']//a)[1]").click
-    #driver.implicitly_wait(20)
-    driver.get("https://www.cnn.com/2023/05/10/politics/governor-bill-lee-tennessee-school-shooting/index.html")
+    # driver.get("https://www.cnn.com/POLITICS/")
+    # driver.find_element(By.XPATH, "(//h3[@class='cd__headline']//a)[1]").click
+    # driver.implicitly_wait(20)
+    driver.get(
+        "https://www.cnn.com/2023/05/10/politics/governor-bill-lee-tennessee-school-shooting/index.html")
     soup = BeautifulSoup(driver.page_source, "html.parser")
-    driver.quit()
     article_parts = ""
     for x in soup.find_all("p", class_="paragraph"):
         article_parts += x.get_text()
     if "This story has been updated with additional information." in article_parts:
-        article_parts = article_parts.replace("This story has been updated with additional information.", "")
+        article_parts = article_parts.replace(
+            "This story has been updated with additional information.", "")
     talk_to_chatgpt(article_parts, driver)
 
+
 def talk_to_chatgpt(article, driver):
-    # https://github.com/Zeeshanahmad4/Automating-ChatGPT-with-Python-and-Selenium/
     driver.get("https://chat.openai.com")
-    time.sleep(3)
+    time.sleep(4)
     input_field = driver.find_element(By.TAG_NAME, "textarea")
-    input_field.send_keys(f'Can you write a one paragraph summary of this article: "{article}"')
+    js = f"""
+    document.getElementsByTagName('textarea')[0].value = `
+    Can you write a one paragraph summary of this article: '{article}'
+    `
+    """ 
+    driver.execute_script(js)
     input_field.send_keys(Keys.RETURN)
-    time.sleep(15)
-    summary = driver.find_element(By.CLASS_NAME, "c-message__body").text
-    print(summary)
+    time.sleep(10)
+    js = "document.getElementsByTagName('textarea')[0].value = 'Now write an opinion on that article from the perspective of an 11th grader without mentioning that you're an 11th grader'"
+    driver.execute_script(js)
+    input_field.send_keys(Keys.RETURN)
+    time.sleep(10)
+    js = "document.getElementsByTagName('textarea')[0].value = 'Now write a question you have about the events of the article'"
+    driver.execute_script(js)
+    input_field.send_keys(Keys.RETURN)
+    time.sleep(10)
+    summary = driver.find_element(By.XPATH, "(//div[contains(@class,'markdown prose')]//p)[1]").text 
+    opinion = driver.find_element(By.XPATH, "(//div[contains(@class,'markdown prose')]//p)[2]").text 
+    question = driver.find_element(By.XPATH, "(//div[contains(@class,'markdown prose')]//p)[3]").text 
+    
 
 def write_out_events():
     driver = create_webdriver()
